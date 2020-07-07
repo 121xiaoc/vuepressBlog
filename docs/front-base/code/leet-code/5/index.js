d4dab8073837e155cab3442a24c1b733f8ab313a
121xiaoc <1218969916@qq.com>
2020-07-07 20:09:13 +0800

    submit

--- docs/front-base/code/leet-code/5/index.js
+++ docs/front-base/code/leet-code/5/index.js
@@ -1,13 +1,12 @@
 
 var longestPalindrome = function(s) {
-  console.log(1)
-  for(var i = 0; i < s.length; i++){
-    for(var j = 0; j <= i; j++) {
-      var str = s.substring(j, s.length - i + j)
-      if(str === str.split('').reverse().join('')) {
-        return str
-      }
+  var point = []
+  for(var i = 0; i < s.length; i++) {
+    point[i][i] = true
+  }
+  for(var j = 0; j < s.length; j ++) {
+    for(var i = 0; i < j - 1; j ++) {
+      
     }
   }
-  return ''
 };
